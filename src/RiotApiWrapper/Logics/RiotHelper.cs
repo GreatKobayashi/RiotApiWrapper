0a917cc9d29181c78063ab74c6ebef5c7da5e913
DESKTOP-V8DLB94\kobay <kobayashiryu123@gmail.com>
2025-04-29 13:46:58 +0900

    Regionのマッピング追加

---
+++ src/RiotApiWrapper/Logics/RiotHelper.cs
@@ -0,0 +1,37 @@
+﻿using RiotApiWrapper.Misc;
+
+namespace RiotApiWrapper.Logics
+{
+    public static class RiotHelper
+    {
+        public static Dictionary<Platform, Region> PlatformRegionPairs = new() {
+            { Platform.BR1, Region.Americas },
+            { Platform.EUN1, Region.Europe},
+            { Platform.EUW1, Region.Europe},
+            { Platform.JP1,  Region.Asia },
+            { Platform.KR,   Region.Asia },
+            { Platform.LA1,  Region.Americas },
+            { Platform.LA2,  Region.Americas },
+            { Platform.ME1,  Region.Asia },
+            { Platform.NA1,  Region.Americas },
+            { Platform.OC1,  Region.SEA },
+            { Platform.RU,   Region.Europe },
+            { Platform.SG2,  Region.SEA },
+            { Platform.TR1,  Region.Europe },
+            { Platform.TW2,  Region.SEA },
+            { Platform.VN2,  Region.SEA }
+        };
+
+        public static Region GetRegionFromPlatform(Platform platform)
+        {
+            if (PlatformRegionPairs.TryGetValue(platform, out Region region))
+            {
+                return region;
+            }
+            else
+            {
+                throw new ArgumentException($"No matching region found for platform: {platform}");
+            }
+        }
+    }
+}
